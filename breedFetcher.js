e6102cc532e3d43c194681ae6029690b791806f8
Karolina Redden <dubajkaro@gmail.com>
2022-02-24 00:34:03 +0000

    edge cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -9,15 +9,24 @@ if (args.length === 0) {
 
 const link = `https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`;
 request(link, (error, response, body) => {
-  // console.log(typeof body);
-  // console.log("this is the body inside the request:", body);
-  const data = JSON.parse(body);
-  console.log(data);
-  //console.log(typeof data);
-  //console.log(data[0]);
-  console.log(data[0].description);
-
+  if (error) {
+    console.log(error);
+    process.exit(1);
+  }
+  if (response.statusCode === 200) {
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      console.log("No cat found");
+    } else {
+      console.log(data[0].description);
+    }
+  }
 });
+
+
+
+
+
 
 
 
